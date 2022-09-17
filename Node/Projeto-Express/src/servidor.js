5672ddcba7f09e8380c4e17cbee8fcbf40f39b82
Hayla Gabriele <82326458+Haylagabriele@users.noreply.github.com>
2022-09-17 19:12:00 -0300

    req,res,next

--- Node/Projeto-Express/src/servidor.js
+++ Node/Projeto-Express/src/servidor.js
@@ -2,14 +2,39 @@
 const porta = 3031
 const express= require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
 
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.use((req,res,next)=>{
-  res.send({nome:'Notebook',preco:123.45})
+app.get('/produtos',(req,res,next)=>{
+  res.send(bancoDeDados.getProdutos())
 })
+app.get('/produtos/:id',(req, res, next)=>{
+  res.send(bancoDeDados.getProduto(req, res, next))
+})
+
 app.post('/produtos',(req,res,next)=>{
-  res.send({nome:'Notebook',preco:123.45})
+  const produto = bancoDeDados.salvarProduto({
+    nome: req.body.nome,
+    preco: req.body.preco 
+  })
+  res.send(produto)
 })
+
+app.put('/produtos/:id',(req,res,next)=>{
+  const produto = bancoDeDados.salvarProduto({
+    id: req.params.id,
+    nome: req.body.nome,
+    preco: req.body.preco 
+  })
+  res.send(produto)
+})
+app.delete('/produtos/:id',(req,res,next)=>{
+  const produto = bancoDeDados.excluirProduto(req.params.id)
+  res.send(produto)
+})
+
 app.listen(porta,()=>{
   console.log(`Servidor está funcionando na porta ${porta}.`)
 })
